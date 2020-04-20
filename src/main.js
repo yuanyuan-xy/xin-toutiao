415aa2a67cf603af07f2e9ecee6ab85cf128fd6b
yuanyuan <1401389760@qq.com>
2020-04-20 14:31:11 +0800

    引入element

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router'
 
 import './styles/index.less'
 
+// 导入element-ui
+import ElementUI from 'element-ui'
+
+// 导入element的css样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 注册全局element
+Vue.use(ElementUI)
+
+// 关闭开发模式下的错误提醒
 Vue.config.productionTip = false
 
 new Vue({
